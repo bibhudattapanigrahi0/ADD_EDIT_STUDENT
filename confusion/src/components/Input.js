e4214eb10708785aa62fd29579a595a99a3ec159
aditya2866 <44620478+aditya2866@users.noreply.github.com>
2020-11-02 14:36:23 +0530

    Update Input.js

--- confusion/src/components/Input.js
+++ confusion/src/components/Input.js
@@ -15,6 +15,12 @@ import Fb_student_feedback from './forms/fb_student_feedback';
 import Hs_assigned_student_room from './forms/hs_assigned_student_room'
 import Hs_student_allotment_list from './forms/hs_student_allotment_list'
 import Reg_regular_form from './forms/reg_regular_form'
+import Studentacad from './forms/stu_academic'
+import StudentAdmFee from './forms/stu_adm_fee'
+import StudentInfo from './forms/stu_info'
+import StudentEnrollPassout from './forms/stu_enroll_passout'
+import StuOtherInfo from './forms/stu_other_info'
+import StuPrepData from './forms/stu_prep_data'
 
 class Input extends Component{
   constructor(props){
@@ -45,9 +51,13 @@ render(){
                 <li class="nav-item" ><NavLink className="nav-link"  to='/Fb_student_feedback'>student_feedback</NavLink></li>
                 <li class="nav-item" ><NavLink className="nav-link"  to='/hs_assigned_student_room'>hs_assigned_student_room</NavLink></li>
                 <li class="nav-item" ><NavLink className="nav-link"  to='/hs_student_allotment_list'>hs_student_allotment_list</NavLink></li>
-                 <li class="nav-item" ><NavLink className="nav-link"  to='/reg_regular_form'>regular_form</NavLink></li>
-
-
+                <li class="nav-item" ><NavLink className="nav-link"  to='/reg_regular_form'>regular_form</NavLink></li>
+                <li class="nav-item" ><NavLink className="nav-link"  to='/stu_academic'>stu_academic</NavLink></li>
+                <li class="nav-item" ><NavLink className="nav-link"  to='/stu_adm_fee'>stu_adm_fee</NavLink></li>
+                <li class="nav-item" ><NavLink className="nav-link"  to='/stu_info'>stu_info</NavLink></li>
+                <li class="nav-item" ><NavLink className="nav-link"  to='/stu_enroll_passout'>stu_enroll_passout</NavLink></li>
+                <li class="nav-item" ><NavLink className="nav-link"  to='/stu_other_info'>stu_other_info</NavLink></li>
+                <li class="nav-item" ><NavLink className="nav-link"  to='/stu_prep_data'>stu_prep_data</NavLink></li>
                 </ul>
         </div>
     </nav>
@@ -61,17 +71,16 @@ render(){
              <Route exact path='/hs_assigned_student_room' component={() => <Hs_assigned_student_room />} />
              <Route exact path='/hs_student_allotment_list' component={() => <Hs_student_allotment_list />} />
              <Route exact path='/reg_regular_form' component={() => <Reg_regular_form />} />
+             <Route exact path='/stu_academic' component={() => <Studentacad />} />
+             <Route exact path='/stu_adm_fee' component={() => <StudentAdmFee />} />
+             <Route exact path='/stu_info' component={() => <StudentInfo />} />
+             <Route exact path='/stu_enroll_passout' component={() => <StudentEnrollPassout />} />
+             <Route exact path='/stu_other_info' component={() => <StuOtherInfo />} />
+             <Route exact path='/stu_prep_data' component={() => <StuPrepData />} />
              <Redirect to="/Studentform" />
          </Switch>
 
-
     </div>
-
-
-
-
-
-
   );
 
 }
